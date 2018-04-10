285e89dea97504e54f56b563ba8947d4126eb55d
Henry Munoz <alexcol123@yahoo.com>
2018-04-10 16:22:36 -0400

    update commit

---
+++ photowall/src/Components/App.js
@@ -0,0 +1,19 @@
+import Main from './Main'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {removePost} from '../redux/actions'
+import {withRoutre}
+
+function mapStateToProps(state){
+  return{
+    posts: state
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({removePost}, dispatch)
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Main)
+
+export default  App
