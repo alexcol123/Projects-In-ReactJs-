3d9997375da7695b7f8eaeb88c590fff53b0f513
Henry Munoz <alexcol123@yahoo.com>
2018-08-21 12:06:31 -0400

    updatedPhotowall 2.0

--- photowallNew/src/Components/Photo.js
+++ photowallNew/src/Components/Photo.js
@@ -10,7 +10,7 @@ function Photo(props){
 
             <figcaption> <p>{post.description}</p></figcaption>
             <div className="button-container">
-              <button className="remove-button" onClick={()=> {
+              <button  onClick={()=> {
                   props.onRemovePhoto(post)
               }}> Remove </button>
             </div>
--- photowallNew/src/index.js
+++ photowallNew/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from  './Components/Main'
 import './styles/stylesheet.css'
+import  {BrowserRouter} from 'react-router-dom'
 
 
 
@@ -10,4 +11,5 @@ import './styles/stylesheet.css'
 
 
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'));
